485d2bbfdb5a455f345bf2aeb8fa3f1a62e64b8c
omeinsotelo <omeinsotelo@gmail.com>
2020-03-31 15:52:49 -0500

    modify task 6 base, states, city

--- models/state.py
+++ models/state.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from os import getenv
+from sqlalchemy.orm import relationship
+import models
 
 
 class State(BaseModel):
@@ -8,4 +12,22 @@ class State(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "state"
+    name = Column(
+        String(128),
+        nullable=False
+    )
+    if getenv("HBNB_TYPE_STORAGE") == "DBStorage":
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+
+        @property
+        def cities(self):
+            cities_dic = []
+            for key, value in models.storage.all().items():
+                try:
+                    if value.state_id == self.id:
+                        cities_dic.append(value)
+                except BaseException:
+                    pass
+                return cities_dic
