620754dec715c1ee8295d94007a08ab6c0c8c58a
felipesv <1165@holbertonschool.com>
2020-04-01 10:56:43 -0500

    try to fix task 6

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,7 @@
 -- MySQL setup development --
 
+DROP DATABASE IF EXISTS hbnb_dev_db;
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
