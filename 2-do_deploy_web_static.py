d3970f7a3f21cd79924fd729b05b99e1eb9a9ca0
omeinsotelo <omeinsotelo@gmail.com>
2020-04-11 19:13:57 -0500

    fixing 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -39,35 +39,35 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/{}".format(fullName))
 
         # Create the directory where are going to be uncompress
-        command = "mkdir -p /data/web_static/releases/{}/".format(fileName)
+        command = "sudo mkdir -p /data/web_static/releases/{}/".format(fileName)
         run(command)
 
         # Uncompress the file into /data/web_static/releases/
-        command = "tar -xzf /tmp/{} -C ".format(fullName)
+        command = "sudo tar -xzf /tmp/{} -C ".format(fullName)
         command += "/data/web_static/releases/{}/".format(fileName)
         run(command)
 
         # Delete file from server
-        command = "rm /tmp/{}".format(fullName)
+        command = "sudo rm /tmp/{}".format(fullName)
         run(command)
 
         # Move all the files into the dir releases/web_static<number>
-        command = "mv /data/web_static/releases/{}".format(fileName)
+        command = "sudo mv /data/web_static/releases/{}".format(fileName)
         command += "/web_static/* "
         command += "/data/web_static/releases/{}/".format(fileName)
         run(command)
 
         # Delete the folder that create the uncompres process
-        command = "rm -rf /data/web_static/releases/{}".format(fileName)
+        command = "sudo rm -rf /data/web_static/releases/{}".format(fileName)
         command += "/web_static"
         run(command)
 
         # Delete the symbolic link
-        command = "rm -rf /data/web_static/current"
+        command = "sudo rm -rf /data/web_static/current"
         run(command)
 
         # Create the new symbolic link
-        command = "ln -s /data/web_static/releases/{}/ ".format(fileName)
+        command = "sudo ln -s /data/web_static/releases/{}/ ".format(fileName)
         command += "/data/web_static/current"
         run(command)
     except Exception:
